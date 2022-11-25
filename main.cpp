35f7e3fcf5723fb8ecb416535c5819e2041649ab
hnpark <hnpark@4dreplay.com>
2022-11-25 16:08:21 +0900

    code clean up

--- main.cpp
+++ main.cpp
@@ -19,16 +19,12 @@ Mat output = Mat::zeros(400, 400, CV_8UC3);
 
 int main() {
 
-
-
 	// input data
 	vector<Point2d> data;
 	data.push_back(Point2d(-1.0, 0.0));
 	data.push_back(Point2d(-0.5, 0.5));
 	data.push_back(Point2d(0.5, -0.5));
 	data.push_back(Point2d(1.0, 0.0));
-
-
 
 	for (int i = 0; i < data.size(); i++) {
 		circle(output, data[i] * 100 + Point2d(200, 200), 3, Scalar(0, 0, 255), -1);
@@ -38,32 +34,24 @@ int main() {
 	imshow("output", output);
 	waitKey(10);
 
-
 	// set parameter
 	int degree = 2;
 	vector<int> knots{ 0,0,0,1,2,2,2 };
-
-
 	vector<Point2d> point;
 	for (double t = 0; t < 1; t += 0.01) {
-
 		point.push_back(Interpolate(t, degree, data, knots));
 	}
 
 	for (int i = 0; i < point.size(); i++) {
 		circle(output, point[i] * 100 + Point2d(200, 200), 1, Scalar(0, 255, 0), -1);
-
 	}
 	cout << point << endl;
 
 	imshow("output", output);
 	waitKey(0);
 
-
 	data.clear();
-
 	return 0;
-
 }
 
 Point2d Interpolate(double t, int degree, vector<Point2d> points, vector<int> knots) {
@@ -102,66 +90,43 @@ Point2d Interpolate(double t, int degree, vector<Point2d> points, vector<int> kn
 		}
 	}
 
-
 	//convert points to homogeneous coordinates
-	vector<vector<double>> v(n, vector<double>(3,0));
+	vector<vector<double>> v(n, vector<double>(3, 0));
 	for (i = 0; i < n; i++) {
 		for (j = 0; j < d; j++) {
 			if (j == 0)
-				v[i][j]=points[i].x;
+				v[i][j] = points[i].x;
 			else
-				v[i][j]= points[i].y;
+				v[i][j] = points[i].y;
 		}
 	}
 
-
-	//cout << " t " << t << endl;
-
 	double alpha;
-	//vector<Point2d> pt;
 	double tx, ty;
 	double ch;
 
 	for (l = 1; l <= degree + 1; l++) {
 		for (i = s; i > s - degree - 1 + l; i--) {
 			alpha = (t - knots[i]) / (double)(knots[i + degree + 1 - l] - knots[i]);
-			
-			//printf("%f\n", alpha);
 
-			//cout << l << "  " << i << endl;
-			for (j = 0; j < d+1; j++) {
-				
+			for (j = 0; j < d + 1; j++) {
+
 				ch = (1 - alpha) * v[i - 1][j] + alpha * v[i][j];
 				v[i][j] = (1 - alpha) * v[i - 1][j] + alpha * v[i][j];
-				
 
 				if (i == s && j == 2) {
 					cout << s << endl;
 					printf("%f\n", alpha);
 					printf("%f\n", v[i][j]);
 				}
-
-				//printf("%f\n", v[i][j]);
 			}
-
-
-			//tx = (1 - alpha) * points[i - 1].x + alpha * points[i].x;
-			//ty = (1 - alpha) * points[i - 1].y + alpha * points[i].y;
-			////cout << tx << "  " << ty << endl;
-
-			//pt.push_back(Point2d(tx, ty));
-			
-			//circle(output, Point2d(tx,ty) * 100 + Point2d(200, 200), 1, Scalar(0, 255, 0), -1);
 		}
 	}
-	tx = v[s][0] ;
+	tx = v[s][0];
 	ty = v[s][1];
-	cout << v[s][2] << endl;
-	//cout << ty << endl;
-
-	//pt.push_back(Point2d(tx,ty));
+	//cout << v[s][2] << endl;
 
 	//circle(output, pt[t] * 100 + Point2d(200, 200), 1, Scalar(0, 255, 0), -1);
 
-	return Point2d(tx,ty);
+	return Point2d(tx, ty);
 }
